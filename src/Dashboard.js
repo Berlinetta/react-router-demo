896e9201d506618fa429934377affb482c272b90
toby wang <tobyberlinetta@gmail.com>
2018-04-16 18:01:39 +0800

    Add nested router.

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import {Route} from "react-router-dom";
+import Invoice from "./Invoice";
 
 class Dashboard extends Component {
     render() {
         return (
             <div className="App">
                 this is content of dashboard
+                <Route
+                    path={this.props.match.url + '/invoice'}
+                    component={Invoice}
+                />
             </div>
         );
     }
---
+++ src/Invoice.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+//import {Link, Route} from "react-router-dom";
+
+class Invoice extends Component {
+    render() {
+        return (
+            <div className="App">
+                this is content of invoice
+            </div>
+        );
+    }
+}
+
+export default Invoice;
